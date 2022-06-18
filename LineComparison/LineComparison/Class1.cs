b66b2fadeeb32cb32f6cc436ba285ca50ccd5e42
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-18 15:28:33 +0530

    Commiting day9 files

---
+++ LineComparison/LineComparison/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Line1
+    {
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Line1.DisplayMessage();
+        }
+    }
+}
